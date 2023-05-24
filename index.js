11d582be53d80d8181e488efdbe984417961b306
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-24 20:44:20 +0300

    Data types

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log(33564)
-alert(5515)
+console.log(true); // boolean
+console.log('This is text'); // string
+console.log(50); // number
+console.log(undefined);
+console.log(null);
+console.log(5n);
+
+console.log();
+
+/*
+dmvpsdvmpsdvmpsdv
+sdvds
+vsd
+vsdv
+sdv
+sdv
+sd
+vs
+dv
+*/
