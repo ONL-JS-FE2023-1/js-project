e3eae148386234c0a3bebe81300135bb5aa59677
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-25 19:17:16 +0300

    Concatenation

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-const box7 = 123;
-console.log(box7);
+let box1 = 1 + 2;
+console.log(box1);
+let box2 = 2 - 1;
+console.log(box2);
+let box3 = 2 * 2;
+console.log(box3);
+let box4 = 6 / 3;
+console.log(box4);
 
-// box7 = 'test';
+let box5 = 0.1 + 0.2;
+console.log(box5);
 
-console.log('test');
+let box10 = box1 + box2 * box3 - box4 / box5;
+console.log(box10);
+
+let num1 = 1 - 'test';
+console.log(num1);
+
+let notAllowed = 5 / 0;
+console.log(notAllowed);
+let divToInf = 5 / Infinity;
+console.log(divToInf);
+
+
+/* Конкатенація
+
+Складати числа
+Зклеюємо строки (конкатенація)
+
+*/
+
+let newLine = 'Hello' + ' world';
+console.log(newLine);
+
+newLine = 5 + 'Hello';
+console.log(newLine);
+
+let box15 = 1 + 1;
+box15 = 1 + '1';
+console.log(box15);
