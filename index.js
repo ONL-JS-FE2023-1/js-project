da445cf0e0445fe5154862c493f6a04dddadbd1d
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-25 18:19:46 +0300

    Variable welcome

--- index.js
+++ index.js
@@ -1,20 +1,63 @@
-console.log(true); // boolean
-console.log('This is text'); // string
-console.log(50); // number
-console.log(undefined);
-console.log(null);
-console.log(5n);
+/*
 
-console.log();
+Переменная
+Змінна
+Variable
+
+*/
 
 /*
-dmvpsdvmpsdvmpsdv
-sdvds
-vsd
-vsdv
-sdv
-sdv
-sd
-vs
-dv
+Створення змінної
+
+ключове-слово ім'я-змінної
+
+var -- deprecated
+
+let - CD-RW
+const - CR-R
 */
+
+let box; // Оголошення змінної (Variable Declaration)
+
+/* Оператор присвоєння = */
+
+box = 5;
+console.log(box);
+
+let box2 = "Hello";
+console.log(box2);
+
+box2 = 10;
+console.log(box2);
+
+const isActive = false;
+const isLogged = true;
+
+const x = 10;
+let y = x;
+console.log(y);
+
+
+
+/* Правила іменування */
+
+let important variable = 0;
+
+/* CamelCase */
+
+let importantVariable = 0;
+
+let a = 6; // bad practice
+
+// Ім'я змінної змінної має пояснювати, що в ній зберігається
+
+let data; // bad practice
+let value; // bad practice
+
+let _; // lodash
+let $; // jquery
+
+let #value; // private field
+let _value; // private field
+
+let 123value; // Це заборонено
