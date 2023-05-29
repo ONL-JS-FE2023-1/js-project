7d9238fb830cd590d4b1ef2573066afce12b945d
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-29 20:18:48 +0300

    Continue... function

--- index.js
+++ index.js
@@ -16,3 +16,24 @@ greetingUser('Alex'); // Alex - аргумент функції
 greetingUser('Ivan');
 greetingUser('Kolya');
 greetingUser('Simon');
+
+/*
+Створіть функцію, яка приймає число, додає до нього 5 і виводить результат на консоль
+*/
+
+function addFiveToNumber(number) {
+    let result = number + 5;
+    console.log(result);
+}
+
+addFiveToNumber(15);
+
+/* Задача: написати функцію, яка складає 2 числа і повертає результат розрахунку */
+
+function addTwoNumbers(num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+
+let functionResult = addTwoNumbers(2, 4);
+console.log(functionResult);
