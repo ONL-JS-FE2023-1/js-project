b39d1eac61d347e710c2e64d241bf2d296196b76
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-29 19:52:56 +0300

    Function welcome

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-let number1 = Number(prompt('Введіть перше число')); // 1: prompt -> '5' ; 2: Number('5') -> 5
-let number2 = Number(prompt('Введіть друге число')); // 1: prompt -> '10' ; 2: Number('10') -> 10
-let result = number1 + number2;
+/* Об'явлення функції
 
-if(Number.isNaN(result)) {
-    console.log('Ти не правий, введи число');
-} else {
-    console.log(result);
+function ім'я_функції(argument1, argument2) {
+    // тіло функції
 }
+
+*/
+
+function greetingUser(username) {
+    console.log('Привіт, ' + username);
+    // ... 100 рядків коду
+}
+
+/* Використання функції */
+greetingUser('Alex'); // Alex - аргумент функції
+greetingUser('Ivan');
+greetingUser('Kolya');
+greetingUser('Simon');
