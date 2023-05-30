30c49f7fb1f3a72a5ecb76bfc3356eec177ae928
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-30 21:05:46 +0300

    High Order Function

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+/*
+
+High Order Function - Функція вищого порядку - це функція, яка приймає іншу функцію в якості аргумента
+
+Callback - Колбек (функція зворотнього виклику) - це функція, ЯКУ передали як аргумент іншій функції
+
+*/
+
 function sum(a, b) {
     return a + b;
 }
 
-const superFunction = sum;
+function div(a, b) {
+    return a / b;
+}
 
-sum(2, 2); // 4
-superFunction(2, 3); // 5
+function sub(a, b) {
+    return a - b;
+}
 
+function multiply(a, b) {
+    return a * b;
+}
+
+function doingSomething(arg1, arg2, fun) { // HOF 
+    const result = fun(arg1, arg2);
+    return result;
+}
+
+doingSomething(2, 4, sum); // 6
+doingSomething(2, 4, sub); // -2
+
+
+// function яка сортує
+// HOF
+
+doingSomething(3, 5, function (a, b) {
+    return a*b;
+})
