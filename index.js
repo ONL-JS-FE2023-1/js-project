5a22099f2581696ead6c12bea99b417b9a4fa31c
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-30 19:06:28 +0300

    Practice task resolve

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ const result = умова ? операнд1 : операнд2;
 const age = 5;
 const isAdult = age < 16 ? 'not adult' : 'adult'; // ... ? false : true
 console.log(isAdult);
+
+// Задача:
+
+/*
+
+1. Створіть булеву змінну isAuth, в якій самостійно визначте, чи авторизований користувач, чи ні. 
+Тобто привласніть цій змінній значення: або true або false.
+2. Створіть змінну viewMode, яка у випадку авторизованого користувача має містити рядок 'authMode',
+а у випадку неавторизованного - 'unAuth'
+
+*/
+
+const isAuth = true;
+const viewMode = isAuth ? 'authMode' : 'unauth'; // isAuth === true
