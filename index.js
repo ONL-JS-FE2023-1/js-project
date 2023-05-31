6437ad19ae1dbccc4ce4b8e3a7896496a4d46b85
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-31 18:07:49 +0300

    HOF example

--- index.js
+++ index.js
@@ -1,29 +1,119 @@
-// task 1
+function washingFace() {
+    // ............
+    console.log('Washing the face');
+}
 
-const userChoice = prompt('Оберіть напій:\n 1 - Сік,\n 2 - Вода,\n 3 - Кава,\n 4 - Чай,\n 5 - Лимонад');
+function brushingTeeth() {
+    // ............
+    console.log('Brushing teeth');
+}
 
-switch(userChoice) {
-    case '1': {
-        console.log('Сік');
-        break;
-    }
-    case '2': {
-        console.log('Вода');
-        break;
-    }
-    case '3': {
-        console.log('Кава');
-        break;
-    }
-    case '4': {
-        console.log('Чай');
-        break;
-    }
-    case '5': {
-        console.log('Лимонад');
-        break;
-    }
-    default: {
-        console.log('Оберіть щось із меню!');
+function skincare() { // догляд за шкірою
+    // ............
+    console.log('Skincare');
+}
+
+function coding() {
+    // ............
+    console.log('Coding');
+}
+
+function sleep() {
+    // ............
+    console.log('Sleep');
+}
+
+function goToWork() {
+    // ............
+    console.log('Go to work');
+}
+
+function goToHome() {
+    // ............
+    console.log('Go home');
+}
+
+function breakfast() {
+    // ............
+    console.log('Breakfast');
+}
+
+function education() {
+    // ............
+    console.log('Education');
+}
+
+function lunch() { // Обід
+    // ............
+    console.log('Lunch');
+}
+
+function dinner() {
+    // ............
+    console.log('Dinner');
+}
+
+function walk() { // Прогулянка
+    // ............
+    console.log('Walk');
+}
+
+function shower() {
+    // ............
+    console.log('Shower');
+}
+
+function todo(timeOfDay) { // Higher-order function
+    switch (timeOfDay) {
+        case 'morning': {
+
+            console.log('---->>>> MORNING <<<<----');
+            washingFace();
+            brushingTeeth();
+            skincare();
+            walk();
+            breakfast();
+
+
+
+            break;
+        }
+        case 'lunch': {
+
+            console.log('---->>>> LUNCH <<<<----');
+            goToWork();
+            coding();
+            lunch();
+            coding();
+            walk();
+            goToHome();
+
+
+
+            break;
+        }
+        case 'evening': {
+
+            console.log('---->>>> EVENING <<<<----');
+            education();
+            dinner();
+            coding();
+            shower();
+            brushingTeeth();
+            washingFace();
+            skincare();
+            sleep();
+
+
+
+            break;
+        }
+        default: {
+            console.log('Error! Enter a valid time of day!');
+        }
     }
 }
+
+todo('morning');
+todo('lunch');
+todo('evening');
