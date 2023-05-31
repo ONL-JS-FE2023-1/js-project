7c7b3be681e3435c9f5abd9a4a982d29aa7e93d6
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-31 19:37:59 +0300

    Loop: do-while

--- index.js
+++ index.js
@@ -1,58 +1,16 @@
-// Loops - Цикли
+// do..while
 
 /*
 
-while (умова) {
-    код повторюється, поки умова = true
-}
+do {
+    тут буде блок коду, який повторювано виконуватись
+} while(умова)
 
 */
 
-// let iterator = 0;
+let pass = '1234';
+let userPass;
 
-// /* Прохід циклу (коло) - ітерація */
-// while (iterator < 10) {
-//     console.log(iterator);
-//     iterator++;
-// }
-
-
-while (true) {
-    console.log('test');
-    break;
-}
-
-// Задача: зробити 10 повторів циклу, вивести на консоль парні значення лічильнику
-
-let iterator = 0;
-while(iterator < 10) {
-    if(iterator % 2 === 0) {
-        console.log(iterator);
-    }
-
-    iterator++;
-}
-
-/*
-
-password = qwerty1234;
-
-Запитуйте у користувача пароль через prompt, поки він не співпаде з тим, що лежить в змінній
-
-*/
-
-let pass = 'qwerty1234';
-
-// variant 1
-// let userPass = prompt('Введіть свій пароль');
-// while(userPass !== pass) {
-//     userPass = prompt('Невірний пароль! Введіть ще раз!');
-// }
-
-// variant 2
-while(true) {
-    let userInput = prompt('Введіть свій пароль');
-    if(userInput === pass) {
-        break;
-    }
-}
+do {
+    userPass = prompt('Ввведіть свій пароль');
+} while (userPass !== pass)
