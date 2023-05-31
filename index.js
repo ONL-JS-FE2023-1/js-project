18c2ba0053aa2b7c5b49e5425498de529ae958c4
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-31 20:44:32 +0300

    Objects

--- index.js
+++ index.js
@@ -1,16 +1,83 @@
-// do..while
-
 /*
 
-do {
-    тут буде блок коду, який повторювано виконуватись
-} while(умова)
+Objects
+
+Об'єкти мають:
+- властивості
+- методи
 
 */
 
-let pass = '1234';
-let userPass;
+let obj = {
+    color: 'red',
+    fontSize: 2,
+    'favorite food': 'fish'
+}
 
-do {
-    userPass = prompt('Ввведіть свій пароль');
-} while (userPass !== pass)
+/*
+
+Об'єкт Автобіль
+
+Властивості
+- марка
+- модель
+- рік випуску
+- швидкість
+
+Методи
+- прискоритись
+- гальмувати
+- змінити напрямок
+
+*/
+
+/*
+
+Об'єкт банківський рахунок
+
+Властивості
+- номер рахунку
+- баланс
+- власник
+- тип рахунку
+
+Методи
+- зняти гроші
+- поповнити рахунок
+- переказати кошти
+
+*/
+
+let bulb = {
+    lightness: 200,
+    power: '200W',
+    cap: 'small',
+    on: function() {
+        return 'The light on!'
+    }
+}
+
+console.log(bulb.lightness);
+
+console.log(bulb.on());
+
+let dog = {
+    name: 'Tuzik',
+    'favorite food': 'meat'
+}
+
+console.log(dog['favorite food']);
+
+
+// Створення об'єктів
+
+// 1
+const cat = {
+
+} // літеральне створення об'єкта
+
+// 2
+const cat2 = Object(); // функція-конструктор
+
+// 3
+const cat3 = new Object(); // функція-конструктор з оператором new
