975a6e3026247daed3d50943e90ba557260a037d
Roman Ratushnyi <freshcode1001@ukr.net>
2023-05-31 18:18:25 +0300

    Determine function

--- index.js
+++ index.js
@@ -1,119 +1,23 @@
-function washingFace() {
-    // ............
-    console.log('Washing the face');
+function sum(a, b) {
+    return a + b;
 }
 
-function brushingTeeth() {
-    // ............
-    console.log('Brushing teeth');
+/*
+
+Детермінова функція - функція, яка при одних і тих же аргументах повертає один і той самий результат
+
+*/
+
+sum(2, 2); // 4
+
+const variable = 5;
+
+function addToNumberSomeVariable(a) { 
+    return a + variable;
 }
 
-function skincare() { // догляд за шкірою
-    // ............
-    console.log('Skincare');
-}
+console.log(addToNumberSomeVariable(5));
 
-function coding() {
-    // ............
-    console.log('Coding');
-}
+variable = 20;
 
-function sleep() {
-    // ............
-    console.log('Sleep');
-}
-
-function goToWork() {
-    // ............
-    console.log('Go to work');
-}
-
-function goToHome() {
-    // ............
-    console.log('Go home');
-}
-
-function breakfast() {
-    // ............
-    console.log('Breakfast');
-}
-
-function education() {
-    // ............
-    console.log('Education');
-}
-
-function lunch() { // Обід
-    // ............
-    console.log('Lunch');
-}
-
-function dinner() {
-    // ............
-    console.log('Dinner');
-}
-
-function walk() { // Прогулянка
-    // ............
-    console.log('Walk');
-}
-
-function shower() {
-    // ............
-    console.log('Shower');
-}
-
-function todo(timeOfDay) { // Higher-order function
-    switch (timeOfDay) {
-        case 'morning': {
-
-            console.log('---->>>> MORNING <<<<----');
-            washingFace();
-            brushingTeeth();
-            skincare();
-            walk();
-            breakfast();
-
-
-
-            break;
-        }
-        case 'lunch': {
-
-            console.log('---->>>> LUNCH <<<<----');
-            goToWork();
-            coding();
-            lunch();
-            coding();
-            walk();
-            goToHome();
-
-
-
-            break;
-        }
-        case 'evening': {
-
-            console.log('---->>>> EVENING <<<<----');
-            education();
-            dinner();
-            coding();
-            shower();
-            brushingTeeth();
-            washingFace();
-            skincare();
-            sleep();
-
-
-
-            break;
-        }
-        default: {
-            console.log('Error! Enter a valid time of day!');
-        }
-    }
-}
-
-todo('morning');
-todo('lunch');
-todo('evening');
+console.log(addToNumberSomeVariable(5));
