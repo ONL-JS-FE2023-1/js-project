3b7b8ba78567d8c3906b974b1def0448be56479e
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-01 20:06:36 +0300

    Constructors

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const Murzik = {
-    nickname: 'Murzik',
-    '1color': 'Black',
-    breed: 'Siam',
-    age: 1,
-    test: 'test',
-    eat: function() {
-        return 'I am eating';
-    },
-    sleep: function() {
-        return 'I am sleeping';
-    },
+/*
+
+1. Ім'я з великої літери
+2. Функція-конструктор повинна виконуватись лише за допомогою операртора new
+
+*/
+
+// Функція-конструктор
+
+function Cat(name, color, breed, age) {
+    this.name = name; // this - посилання на ось цього новоствореного кота
+    this.color = color;
+    this.breed = breed;
+    this.age = age;
+    this.run = function() {
+        return this.name + ' is running!';
+    }
 }
 
-
-Murzik.friend = {
-    name: 'Barsik',
-    color: 'White',
-    age: 2
-}
+const cat1 = new Cat('Bublik', 'red', 'siam', 2);
+const cat2 = new Cat('Murzik', 'black', 'siam', 4);
+const cat3 = new Cat('Barsik', 'white', null, 4);
