bbed5be064e2ce76f0d208d83b6ac7bcb6cc4c67
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-02 18:55:27 +0300

    Practice task resolve

--- index.js
+++ index.js
@@ -1,30 +1,19 @@
-//alert('2+2=' + (2+2));
-//          2:  1:/4
-alert(`2+2 = ${2 + 2}`);
 
+// У нас є функція
+function greetingUser(user) {
+    return `Hey ${user.name} ${user.surname}. You will be ${user.age + 1} next year`
+}
 
-// 2+2=4
+// та у нас є об'єкт
+const user = {
+    name: 'John',
+    surname: 'Doe',
+    age: 35
+}
 
-// 1 Одинарні лапки - ''
-// 2 Подвійні лапки - ""
-// 3 Зворотні лапки - ``
+/*
+Задача:
+Написати функцію greetingUser таким чином, щоб вона привітала користувача та вказала,
+скільки йому років має виповнитись в наступному році
 
-const name1 = 'Вася';
-const name2 = 'Ігор';
-const name3 = 'Дарія';
-// alert('Привіт, ' + name); // Привіт, Вася
-// alert('Привіт ' + name1 + ', ' + name2 + ', ' + name3 + '!');
-alert(`Привіт, ${name1}, ${name2}, ${name3}!`); // Привіт Вася, Ігор, Дарія!
-// шаблонний рядок
-
-// Задача: запитати у користувача 2 числа, додати їх, та вивести відповідь алертом
-
-const one = Number(prompt('First number'));
-const two = Number(prompt('Second number'));
-
-// alert('-> ' + one + '+' + two + '=' + (one+two));
-
-alert(`-> ${one}+${two}=${one + two}`);
-// -> 2+2=4
-
-console.log(`Привіт, ${name1}`);
+*/
