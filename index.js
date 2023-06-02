0a224b179857d1b6c5845d34f6d48e609dac6643
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-02 19:37:29 +0300

    Another example of how computing properties work

--- index.js
+++ index.js
@@ -1,35 +1,11 @@
-// [] - Обчислювальні властивості
+// Задача: вивести користувачу привітання, залежно від значення, яке користувач ввів
 
-const user = {
-    'name': 'John',
-    'favorite color': 'red',
-    '0': 'value'
+const greets = {
+    1: 'Hi',
+    2: 'Ya, whats up',
+    3: 'Hello',
+    4: 'Good morning'
 }
 
-
-/*
-
-Ключем може бути рядок або тип даних Symbol
-
-*/
-
-// . - оператор доступу за ім'ям властивості
-
-user.name // 'John'
-// user.'favorite color' // syntax error
-// user.0 // syntax error
-
-// [] - Оператор доступу до обчислюваної властивості
-user['favorite color']; // 'red'
-user[0]; // 'value'
-
-
-
-
-const someObj = {
-    2: 10
-}
-
-// someObj.2 // syntax error
-someObj[2] // 10
-someObj['2'] // 10
+const valueFromUser = 3;
+console.log(greets[valueFromUser]);
