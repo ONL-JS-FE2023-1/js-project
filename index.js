0d3e9dc7e35316eb9042bd69914a7a02afd6d943
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-02 18:36:14 +0300

    `` field

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-/*
+//alert('2+2=' + (2+2));
+//          2:  1:/4
+alert(`2+2 = ${2 + 2}`);
 
-Функція-конструктор Country, яка приймає в якості аргументів:
 
-Властивості:
-- назва країни
-- популяція
-- площа
+// 2+2=4
 
-Метод:
-- плотність населення: популяція цієї країни / площу цієї ж країни
+// 1 Одинарні лапки - ''
+// 2 Подвійні лапки - ""
+// 3 Зворотні лапки - ``
 
-*/
+const name1 = 'Вася';
+const name2 = 'Ігор';
+const name3 = 'Дарія';
+// alert('Привіт, ' + name); // Привіт, Вася
+// alert('Привіт ' + name1 + ', ' + name2 + ', ' + name3 + '!');
+alert(`Привіт, ${name1}, ${name2}, ${name3}!`); // Привіт Вася, Ігор, Дарія!
+// шаблонний рядок
 
-function Country(name, population, area) {
-    this.name = name;
-    this.population = population;
-    this.area = area;
+// Задача: запитати у користувача 2 числа, додати їх, та вивести відповідь алертом
 
-    this.getDensity = function() {
-        // const result = this.population / this.area; 
-        return this.population / this.area;
-    }
-}
+const one = Number(prompt('First number'));
+const two = Number(prompt('Second number'));
 
-const UK = new Country('United Kingdom', 67.33, 243610);
+// alert('-> ' + one + '+' + two + '=' + (one+two));
 
-console.log(UK.getDensity());
+alert(`-> ${one}+${two}=${one + two}`);
+// -> 2+2=4
+
+console.log(`Привіт, ${name1}`);
