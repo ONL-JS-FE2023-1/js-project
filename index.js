8cac544221badaa1f4e99b73ed18368b0626592e
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-05 20:45:40 +0300

    Array methods (begin)

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
-const arr = [3, 6, 8, 2, 3, 5, 1];
+const array = [1,2,3];
+const array2 = [3,4,5,6];
+const array3 = ['string1', 'string2'];
 
-function maxArray(array) {
-    let max = array[0];
+// concat
+// array.concat(array1, array2, arrayN);
 
-    for(let i = 0; i < array.length; i++) {
-        if(array[i] > max) {
-            max = array[i];
-        }
-    }
+const test = array.concat(array2, array3);
 
-    return max;
-}
 
-console.log(maxArray(arr));
+// fill
+// array.fill(value, start, end);
+// value - чим заповнюємо масив
+// [start] - звідки очинаємо заповнення
+// [end] - до якого елементу заповнюємо масив (не включаючи)
+const array4 = new Array(5);
+array4.fill(1);
+
+
+// includes
+// array.includes(value, fromIndex);
+// value - що ми шукаємо
+// [fromIndex] - звідки (з якого індексу) ми починаємо пошук
+const array5 = [12,4,15,6,12,12,'ashdhsadlkj'];
+console.log(array5.includes(6));
+
+// indexOf
+// array.indexOf(value, fromIndex)
+// value - що ми шукаємо
+// fromIndex - початковий індекс, з якого ми починаємо пошук
+console.log(array5.indexOf(12));
+
+const array6 = [undefined];
+console.log(array6.indexOf(undefined));
