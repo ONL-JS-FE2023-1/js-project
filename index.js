9a9bc10f090fecb84e43a2d0ce07db5e8c2e8269
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-05 17:54:20 +0300

    Home practice task resolve

--- index.js
+++ index.js
@@ -20,7 +20,36 @@ obj = {
 */
 
 function task1(obj) {
-    for(let key in obj) {
+    for (let key in obj) {
         console.log(`"${key}": *${obj[key]}*`);
     }
+}
+
+
+/*
+
+Task2:
+
+Написати функцію, яка повертає загальну суму, витрачену на зарплатню всіх відділів
+
+*/
+
+const departmentSalary = {
+    HR: 20000,
+    development: 1000000,
+    drive: 300,
+    assistant: 5000,
+    marketing: undefined
+}
+
+function sumSalary(salaryObj) {
+    let result = 0;
+
+    for(let key in salaryObj) {
+        if(typeof salaryObj[key] === 'number') {
+            result = result + salaryObj[key];
+        }
+    }
+
+    return result;
 }
