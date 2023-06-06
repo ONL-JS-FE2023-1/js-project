fa524edf4be59942fdf5a32148fdb75da8c80c7a
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-06 19:23:15 +0300

    Mutations

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
-const array = [1, 2, 3, 4, 5];
+const user = {
+    fullname: 'Humpty Dumptu'
+}
 
-// slice
-// Копіює масив з start до end (не включаючи)
-// array.slice(start, end);
-// start - звідки ми починаємо робити копію
-
-const array2 = array.slice(2);
+user.isBroken = false;
 
 
-const users = [
-    {
-        name: 'John',
-        lastName: 'Doe'
-    },
-    {
-        name: 'Jane',
-        lastName: 'Doe'
-    },
-    {
-        name: 'Jack',
-        lastName: 'Doe'
-    }
-]
+// string
+// number
+// boolean
+// null
+// undefined
+// symbol
+
+const egg = 'Humpty Dumptu';
+egg.isBroken = false;
