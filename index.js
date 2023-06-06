de773428e16d66bbe22af67ee3dba502c8b599fe
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-06 18:39:38 +0300

    Array methods (end)

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+// join
+// array.join(separator)
+const arr1 = ['Hello', 'world', 4, 2, 'hi'];
+console.log(arr1.join(' ')); // default separator - ','
 
-// Задача: написати функцію пошуку середнього значення в масиві чисел
+// push
+// array.push(element1, element2, .....)
+const arr2 = [2, 3, 4, 5];
+arr2.push(2);
+arr2.push('hi', 'hello', 'kadjlk');
 
-const arr = [1, 2, 3];
+// pop
+// array.pop()
+const arr3 = [5, 6, 7, 8];
+const result = arr3.pop(213);
 
-function average(array) {
-    let sum = 0;
+// shift
+// array.shift()
+const arr4 = [12, 13, 14];
+const firstElement = arr4.shift();
 
-    for(let i = 0; i < array.length; i++) {
-        sum += array[i];
-    }
-    
-    return sum / array.length;
-}
+// unshift
+// array.unshift(element1, element2, .....)
+const arr5 = [15, 16];
+arr5.unshift(1, 2, 3, 'hi');
+
+// reverse
+// array.reverse()
+const arr6 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+arr6.reverse();
