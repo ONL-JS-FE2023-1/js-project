f43bee5aa7137c8fe4c398aaba72152fb5d1d554
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-07 20:28:48 +0300

    Array sort

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-// Чейнінг функцій
+// sort
+// array.sort([compareFunction])
 
-// Задача: новий масив, кожен елемент якого = елемент зі старого масиву * 2
+const array = [5, 3, 9, 21, 5, 1, 2, 4];
+array.sort(); // default sort
 
-const array = [2, 3, 4, 5, 1, 2];
+const arrayWords = ['bbb', 'abb', 'aaa', 'aba'];
+arrayWords.sort(); // default sort
 
-const newArray = array.map((item) => {
-    return item * 2;
-}).reverse().join('<<--->>'); // chaining
+function compareFunction(a, b) { // функція-компарартор
+    // if(b > a) {
+    //     return -1;
+    // } else {
+    //     return 1;
+    // } 
+    // сортування за зростанням (варіант 1)
 
+    // return a - b; // сортування у порядку зростання (варіант 2)
 
-// forEach version
-// const newArrayVersion2 = [];
-// array.forEach((item) => {
-//     newArrayVersion2.push(item * 2);
-// })
+    return b - a; // сортування у порядку спадання
+}
+
+array.sort(compareFunction);
+
+/*
+
+returning:
+-1 - спочатку a, потім b
+0 - елементи залишаються на місці
+1 - спочатку b, потім a
+
+*/
+
+// a і b - два сусідні елементи в масиві
