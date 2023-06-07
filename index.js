393686e497caeadc97103b48f6c852dee5b89518
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-07 19:02:37 +0300

    Map task solution

--- index.js
+++ index.js
@@ -1,36 +1,30 @@
-// forEach
-// array.forEach(callbackFunction)
-
-function square(a) { // приймає число і виводить у консолі квадрат цього числа
-    return a*a;
-}
-
-const array = [1, 2, 3, 4, 5];
-
-// array.forEach(square); // return undefined
-
-// array.forEach((item, index, array) => {
-//     console.log(item * item)
-//     console.log(index);
-//     console.log(array);
-//     console.log('--------------------------');
-// })
-
-// function declaration
-
-const arr = [2, 44, 11, 234, 8, 2, 4, 1];
-
-// arr.forEach((item, index, array) => {
-//     console.log(item + 100);
-// })
-
-// map
-// array.map(square); // return new array
-
-const newArray = array.map(square);
+const users = [
+    {
+        name: 'John',
+        lastName: 'Doe',
+        age: 18,
+        email: 'mail.com'
+    },
+    {
+        name: 'Jane',
+        lastName: 'Doe',
+        age: 20,
+        email: 'mail2.com'
+    },
+    {
+        name: 'Jake',
+        lastName: 'Doe',
+        age: 17,
+        email: 'mail3.com'
+    }
+]
 
 
-// Задача: найти квадрат чисел в массиве, которіе находятся на четном индексе
-const newArray2 = array.map((item, index, array) => {
-    return item * item;
-})
+
+const newUsersArray = users.map((item) => {
+    return {
+        fullname: `${item.name} ${item.lastName}`,
+        age: item.age,
+        email: item.email
+    }
+});
