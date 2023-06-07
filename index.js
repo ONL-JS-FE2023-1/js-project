e0b3ae2db092e1963651c653491b7c5dd1337862
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-07 18:26:00 +0300

    Map method

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // array.forEach(callbackFunction)
 
 function square(a) { // приймає число і виводить у консолі квадрат цього числа
-    console.log(a*a);
+    return a*a;
 }
 
 const array = [1, 2, 3, 4, 5];
@@ -20,6 +20,17 @@ const array = [1, 2, 3, 4, 5];
 
 const arr = [2, 44, 11, 234, 8, 2, 4, 1];
 
-arr.forEach((item, index, array) => {
-    console.log(item + 100);
+// arr.forEach((item, index, array) => {
+//     console.log(item + 100);
+// })
+
+// map
+// array.map(square); // return new array
+
+const newArray = array.map(square);
+
+
+// Задача: найти квадрат чисел в массиве, которіе находятся на четном индексе
+const newArray2 = array.map((item, index, array) => {
+    return item * item;
 })
