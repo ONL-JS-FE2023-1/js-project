85453b403b5ee146dddbb463bb3abe50a4de4aac
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-08 18:52:11 +0300

    MyArray

--- index.js
+++ index.js
@@ -1,112 +1,44 @@
-// task 1
+function MyArray() {
+    this.length = 0;
 
-const catArray = [
-    {
-        name: 'Murzik',
-        color: 'black',
-        weight: 3,
-        age: 2
-    },
-    {
-        name: 'Murka',
-        color: 'grey',
-        weight: 5,
-        age: 3
-    },
-    {
-        name: 'Barsik',
-        color: 'red',
-        weight: 2,
-        age: 1
-    },
-    {
-        name: 'Cookie',
-        color: 'brown',
-        weight: 5,
-        age: 2
-    },
-    {
-        name: 'Stus',
-        color: 'grey',
-        weight: 2,
-        age: 5
-    },
-    {
-        name: 'Fagot',
-        color: 'black',
-        weight: 5,
-        age: 3
-    },
-    {
-        name: 'Kit',
-        color: 'pink',
-        weight: 2,
-        age: 1
-    },
+    // push
+    this.push = function (value) {
+        // v1
+        // this[this.length] = value; // this[key] = value
+        // this.length++;
+        // return this.length;
 
-];
+        // v2
+        for (let i = 0; i < arguments.length; i++) {
+            this[this.length] = arguments[i];
+            this.length++;
+        }
 
-const catNames = catArray.map((cat) => {
-    return cat.name;
-})
+        return this.length;
+    }
 
-// task 2
-
-const users = [
-    {
-        name: 'John',
-        lastName: 'Doe',
-        age: 18,
-        email: 'meil@com'
-    },
-    {
-        name: 'Jane',
-        lastName: 'Doe',
-        age: 20,
-        email: 'meil@com'
-    },
-    {
-        name: 'Josh',
-        lastName: 'Doe',
-        age: 17,
-        email: 'meil@com'
-    },
-    {
-        name: 'Jake',
-        lastName: 'Doe',
-        age: 21,
-        email: 'meil@com'
-    },
-    {
-        name: 'Jackson',
-        lastName: 'Doe',
-        age: 17,
-        email: 'meil@com'
-    },
-]
-
-const newUsers = users.map((user) => {
-    if(user.age >= 18) {
-        return {
-            fullname: `${user.name} ${user.lastName}`,
-            email: user.email
+    // pop
+    this.pop = function () {
+        if (this.length > 0) {
+            let lastItem = this[this.length - 1];
+            delete this[this.length - 1];
+            this.length--;
+            return lastItem;
         }
     }
-}).filter((user) => {
-    if(typeof user === 'object') {
-        return true;
-    } else {
-        return false;
+
+    // forEach
+    this.forEach = function (callback) {
+        for (let i = 0; i < this.length; i++) {
+            callback(this[i], i, this);
+        }
     }
+}
+
+const arr = new MyArray();
+
+arr.push(1, 2, 3, 4, 'test', 5, 6)
+
+arr.forEach((item) => {
+    console.log(item);
 })
-
-// task 3
-const arr = [1, 2, 3, 4, 5, 6];
-
-const sum = arr.reduce((accumulator, currentValue) => {
-    if(currentValue % 2 === 0) {
-        accumulator += currentValue;
-    }
-
-    return accumulator;
-}, 0);
