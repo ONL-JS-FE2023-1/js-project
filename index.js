df9fba257bc853e271619a669f6d749d2f723e9e
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-09 20:37:04 +0300

    Practice task resolve

--- index.js
+++ index.js
@@ -1,40 +1,4 @@
-/*
+const arr1 = [1,2,3,45,5];
+const arr2 = [10,1,13,4,56];
 
-Задача:
-
-Вивести на консоль таблицю множення від 1 до 5 у вигляді:
-
-1x1 = 1
-1x2 = 2
-...
-2x1 = 2
-2x2 = 4
-...
-5x1 = 5
-5x2 = 10
-5x3
-5x4
-5x5
-
-*/
-
-for(let i = 1; i <= 5; i++) { // 1
-    for(let j = 1; j <= 5; j++) { // 1..2..3..4..5
-        console.log(`${i} x ${j} = ${i * j}`);
-    }
-}
-
-const number = [[1,2,3], [4,5,6], [7,8,9]];
-
-function sumArray(array) {
-    let sum = 0;
-    for(let i = 0; i < array.length; i++) {
-        for(let j = 0; j < array[i].length; j++) {
-            sum += array[i][j];
-        }
-    }
-
-    return sum;
-}
-
-console.log(sumArray(number));
+const concatArray = [...arr1, ...arr2];
