b303bdaf625ff134a985cc965d72d9318e2a98df
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-13 18:18:23 +0300

    Practice task resolve

--- index.js
+++ index.js
@@ -1,49 +1,47 @@
-/*
+// task 1
 
-Задача: 
-Написати функцію, яка робить кожне слово у переданому рядку з великої літери
-
-('test testovich'); // Test Testovich
-
-*/
-
-/*
-
-Декомпозиція:
-
-1. Розділити рядок на слова (split)
-2. Зробити першу літеру кожного слова великою
-- Пройтись циклом по кожному елементу масиву слів
-- Першу літеру слова (charAt) зробити великою буквою (toUpperCase)
-- Використати метод slice(1) для отримання підрядку, що починається з другого символу слова
-- Об'єднати першу велику літеру з рештою слова, використовувати оператор +
-3. З'єднати слова знову в рядок
-- Використати метод join(' ') для з'єднання слів масиву в один рядок, розділених пробілами
-4. Повернути новий рядок з великими літерами слів
-
-*/
-
-function capitalizeWords(str) {
-    const words = str.split(' ');
-
-    for(let i = 0; i < words.length; i++) {
-        words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+function truncate(str, strLength) {
+    if(str.length <= strLength) {
+        return str;
     }
 
+    const newStr = str.slice(0, strLength);
+    return newStr + '...';
+}
 
-    return words.join(' ');
+// task 2
+
+// xxx
+// viagra -> Viagra -> VIAgra
+
+// v1
+
+function checkSpam(str) {
+    const lowStr = str.toLowerCase();
+    return lowStr.includes('viagra') || lowStr.includes('xxx');
+}
+
+// v2
+
+function checkSpam2(str) {
+    const spamArray = ['viagra', 'xxx', 'drugs'];
+
+    let flag = false;
+
+    for(let i = 0; i < spamArray.length; i++) {
+        if(str.toLowerCase().includes(spamArray[i])) {
+            flag = true;
+        }
+    }
+
+    return flag;
 }
 
 
-/*
-Тести:
+// task 3
 
+// str(reverse) === str
 
-capitalizeWords('hello world')
-'Hello World'
-capitalizeWords('hello world test testovich')
-'Hello World Test Testovich'
-capitalizeWords('hello to you')
-'Hello To You'
-
-*/
+function palindrom(str) {
+    return str.toLowerCase().split('').reverse().join('') === str.toLowerCase();
+}
