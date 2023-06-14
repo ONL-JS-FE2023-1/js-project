a156fbc3ce76e20dc969e371b26481e2bcffd8da
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-14 20:36:39 +0300

    _private field

--- new_syntax.js
+++ new_syntax.js
@@ -38,6 +38,8 @@ const us2 = new User('Jane', 'Doe', 65);
 */
 
 
+// _
+
 const MIN_ZP = 6000;
 const WORK_DAYS = 21;
 const MIN_RATE = MIN_ZP/WORK_DAYS;
@@ -45,8 +47,33 @@ class Worker {
     constructor(name, lastName, rate = MIN_RATE, days = 0) {
         this.name = name;
         this.lastName = lastName;
-        this.rate = rate;
+        this._rate = rate;
+        if(typeof rate !== 'number' || typeof days !== 'number') {
+            throw new TypeError('Rate and days must be a number!');
+        }
+        if (days < 0 || days > 31) {
+            throw new RangeError('Days must be in 0 to 31')
+        }
+        if(rate < 0) {
+            throw new RangeError('Rate must be a positive number')
+        }
         this.days = days;
+    }
+
+    setRate(value) {
+        if(typeof value !== 'number') {
+            throw new TypeError('Rate must be a number!');
+        }
+
+        if(value < 0) {
+            throw new RangeError('Rate must be a positive number')
+        }
+
+        this._rate = value;
+    }
+
+    getRate() {
+        return this._rate;
     }
 
     getSalary() {
@@ -65,3 +92,46 @@ function displayArguments(a = 10, b = 5) {
 
 //displayArguments() // 10 5
 //displayArguments(100, 200) // 100 200
+
+
+
+/*
+
+Задача: порахувати загальну вагу автомобіля
+
+Клас авто
+Клас паливо
+
+
+*/
+
+class Fuel {
+    constructor(volume, density) {
+        this.volume = volume;
+        this.density = density;
+    }
+
+    getWeight() {
+        return this.volume * this.density;
+    }
+}
+
+const benzin = new Fuel(50, 0.9);
+
+
+const MIDDLE_WEIGHT_PASSENGER = 80;
+class Auto {
+    constructor(name, ownWeight, fuel, passengersCount) {
+        this.name = name;
+        this.ownWeight = ownWeight;
+        this.fuel = fuel;
+        this.passengersCount = passengersCount;
+    }
+
+    // метод, який обраховує вагу власну автомобіля + вага палива
+    getFullWeight() {
+        return this.ownWeight + this.fuel.getWeight() + (this.passengersCount * MIDDLE_WEIGHT_PASSENGER);
+    }
+}
+
+const auto = new Auto('Lexus..', 4000, benzin, 2);
