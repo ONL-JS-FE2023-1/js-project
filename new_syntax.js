5f1c07a821b0e06d6e8550197acf44c9d04ae4c7
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-14 19:54:59 +0300

    Default parameters

--- new_syntax.js
+++ new_syntax.js
@@ -36,3 +36,32 @@ const us2 = new User('Jane', 'Doe', 65);
 4. Ім'я класу пишеться завжди з великої літери
 
 */
+
+
+const MIN_ZP = 6000;
+const WORK_DAYS = 21;
+const MIN_RATE = MIN_ZP/WORK_DAYS;
+class Worker {
+    constructor(name, lastName, rate = MIN_RATE, days = 0) {
+        this.name = name;
+        this.lastName = lastName;
+        this.rate = rate;
+        this.days = days;
+    }
+
+    getSalary() {
+        return `Робітник ${this.name} ${this.lastName} заробив ${this.rate * this.days}$`
+    }
+}
+
+const worker1 = new Worker('John', 'Doe', 300, 12);
+
+
+// Параметри за замовченням 
+
+function displayArguments(a = 10, b = 5) {
+    console.log(a, b);
+}
+
+//displayArguments() // 10 5
+//displayArguments(100, 200) // 100 200
