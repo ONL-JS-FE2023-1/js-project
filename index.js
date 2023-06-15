4f7650687e11486f264e0a4a63685a55e0dd1e6a
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-15 21:05:10 +0300

    Extends example v2

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
-class Animal {
-    constructor(color, nickname, breed) {
-        this.nickname = nickname;
-        this.color = color;
-        this.breed = breed;
+class User {
+    constructor(name, surname, age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
     }
 
-    eat() {
-        return `${this.nickname} is eating`
-    }
-}
-
-const anim = new Animal('test', 'test', 'test');
-
-class Cat extends Animal {
-    constructor(color, nickname, breed, catIsGood) {
-        super(color, nickname, breed);
-
-        this.catIsGood = catIsGood;
-    }
-
-    sleep() {
-        return `${this.nickname} is sleeping`
+    getFullName() {
+        return `${this.name} ${this.surname}`
     }
 }
 
-const cat = new Cat('grey', 'Barsik', 'poroda...', true);
+class Moderator extends User {
+    constructor(name, surname, age) {
+        super(name, surname, age);
+    }
+
+    createPost(header, text) {
+        console.log('Post created')
+    }
+
+    removePost(postId) {
+        console.log('Post removed')
+    }
+
+    ban(userId) {
+        console.log('User is banned')
+    }
+}
+
+class Admin extends Moderator {
+    constructor(name, surname, isAdmin) {
+        super(name, surname, null);
+
+        this.isAdmin = isAdmin;
+    }
+
+    createPost() {
+        console.log('Not allowed')
+    }
+
+    removePost() {
+        console.log('Not allowed')
+    }
+
+    setModerator(userId) {
+        console.log('set moderator')
+    }
+
+    deleteModerator(userId) {
+        // варіант 1: видаляємо профіль модератора
+        // варіант 2: знімаємо з модератора його роль модератора
+        console.log('delete moderator')
+    }
+}
+
+const us1 = new User('John', 'Doe', 59);
+
+const mod = new Moderator('Alex', 'Krane',40);
+
+const adm = new Admin('Jane', 'Doe', true);
