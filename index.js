5f2b1a93bb90cd40e6775749fd26db7d83432c1a
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-15 18:55:23 +0300

    Inbox example

--- index.js
+++ index.js
@@ -1,106 +1,67 @@
-class Car {
-    constructor(brand, model, year, fuel) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-        this.fuel = fuel;
+class EmailInbox {
+    constructor() {
+        this._emails = [];
     }
 
-    get brand() {
-        return this._brand;
+    get emails() {
+        return this._emails;
     }
 
-    set brand(value) {
-        if(typeof value !== 'string') {
-            throw new TypeError('Brand must be a string!');
-        }
-        this._brand = value;
-    }
-
-    get model() {
-        return this._model;
-    }
-
-    set model(value) {
-        if(typeof value !== 'string') {
-            throw new TypeError('Model must be a string!');
-        }
-        this._model = value;
-    }
-
-    get year() {
-        return this._year;
-    }
-
-    set year(value) {
-        if(typeof value !== 'number' || !Number.isInteger(value) || value < 0) {
-            throw new TypeError('Year must be a positive integer')
+    set emails(message) {
+        if(typeof message !== 'object') {
+            throw new TypeError('Message must be object')
         }
 
-        this._year = value;
+        this._emails.push(message);
     }
 
-    get fuel() {
-        return this._fuel;
-    }
-
-    set fuel(value) {
-        if(typeof value !== 'number' || value < 0) {
-            throw new TypeError('Fuel must be a positive number')
+    addEmail(subject, sender, message) {
+        const existingEmail = this.emails.filter(email => {
+            if(email.subject === subject) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+        if(existingEmail.length > 0) {
+            throw new Error('Email with the same subject already exists')
         }
 
-        this._fuel = value;
-    }
-
-    drive(distance) {
-        // припустиме споживання палива на 10 кілометрів
-        const fuelConsumption = 10;
-        const fuelNeeded = distance / fuelConsumption; // обчислення кількості палива, яке необхідне, щоб цю відстань пройти
-
-        if(this.fuel >= fuelNeeded) {
-            this.fuel -= fuelNeeded;
-            console.log(`Подорож на ${distance} км виконана! Палива залишилось: ${this.fuel} л.`)
-        } else {
-            console.log(`Недостатньо палива для здійснення поїздки`)
+        // додаємо цей email до скриньки
+        const email = {
+            subject: subject,
+            sender: sender,
+            message: message
         }
+
+        this.emails = email;
     }
 
-    refuel(amount) {
-        this.fuel += amount;
-        console.log(`Автомобіль заправлено на ${amount} л. Загальна кількість палива: ${this.fuel} л.`)
+    removeEmail(subject) {
+        const filteredEmails = this.emails.filter(email => {
+            if(email.subject !== subject) {
+                return true;
+            } else {
+                return false;
+            }
+        })
+
+        this._emails = filteredEmails;
     }
+
+    // getEmails() {
+    //     return this.emails;
+    // }
 }
 
-const car = new Car('Lexus', 'Model', 2013, 50);
 
-/*
+const inbox = new EmailInbox();
 
+inbox.addEmail('test1', 'test1@gmail.com', 'Test test test');
+inbox.addEmail('test2', 'test2@gmail.com', 'Test test test');
+inbox.addEmail('test3', 'test3@gmail.com', 'Test test test');
+// inbox.addEmail('test3', 'test@gmail.com', 'Test test test'); -> error
 
+inbox.removeEmail('test2');
 
-car.drive(100)
-index.js:62 Подорож на 100 км виконана! Палива залишилось: 40 л.
-undefined
-car
-Car {_brand: 'Lexus', _model: 'Model', _year: 2013, _fuel: 40}
-car.drive(100)
-index.js:62 Подорож на 100 км виконана! Палива залишилось: 30 л.
-undefined
-car.drive(100)
-index.js:62 Подорож на 100 км виконана! Палива залишилось: 20 л.
-undefined
-car.drive(150)
-index.js:62 Подорож на 150 км виконана! Палива залишилось: 5 л.
-undefined
-car.drive(150)
-index.js:64 Недостатньо палива для здійснення поїздки
-undefined
-car.refuel(150)
-index.js:70 Автомобіль заправлено на 150 л. Загальна кількість палива: 155 л.
-undefined
-car.fuel
-155
-car.drive(150)
-index.js:62 Подорож на 150 км виконана! Палива залишилось: 140 л.
-
-
-*/
+console.log(inbox.emails); // getEmails
