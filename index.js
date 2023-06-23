528029e9cf280d71bc99f19201364db3d0f3e315
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-23 20:48:39 +0300

    Closure (another examples)

--- index.js
+++ index.js
@@ -57,3 +57,120 @@ const fnObj = makeCounter2();
 
 
 //////
+
+
+
+function remeberParametr(param) {
+    return function() {
+        return param;
+    }
+}
+
+/*
+
+Задача:
+Написати функцію, яка приймає параметр і повертає іншу функцію, що очікує другий параметр
+друга функція повертає суму двох параметрів
+
+*/
+
+function createAdder(first) {
+    let accumulator = 0;
+    return function(second) {
+        return accumulator += first + second;
+    }
+}
+
+const adder = createAdder(5);
+
+
+/*
+
+
+Створіть функцію isCorrectPassword, яка буде приймати правильний пароль і яка буде повертати внутрішню
+функцію, яка приймає введену строку (також пароль) та повертає значення true, якща введена строка
+(у другій функції) співпадає з паролем, false - якщо ні
+
+*/
+
+function isCorrectPassword(truePassword) {
+    return function(checkedPassword) {
+        if(checkedPassword === truePassword) {
+            return true
+        } else {
+            return false;
+        }
+    }
+}
+
+const checkPassword = isCorrectPassword('123');
+
+checkPassword('abracadabra'); // false
+checkPassword('123'); // true
+
+
+// https://developer.mozilla.org/ua/docs/Web
+// protocol - https
+// ://
+// hostName - developer.mozilla.org
+// path - /ua/docs/Web
+
+const createURL = function(protocol) {
+    return function(hostname) {
+        return function(path) {
+            return `${protocol}://${hostname}/${path}`
+        }
+    }
+}
+
+const rememberHost = createURL('https');
+
+const site1 = rememberHost('test.org');
+const site2 = rememberHost('site.com');
+const site3 = rememberHost('wiki.org');
+
+/*
+
+site2('home')
+'https://site.com/home'
+
+site1('products/smartphones')
+'https://test.org/products/smartphones'
+
+*/
+
+// ---------------------------
+
+function createTodoList() {
+    const tasks = []; // массив з завданнями у замиканні
+
+    return {
+        viewTasks() {
+            return tasks;
+        },
+        addTask(task) {
+            tasks.push(task);
+            console.log('Додано завдання: ', task);
+        },
+        removeTask(taskText) {
+            const index = tasks.indexOf(taskText);
+            if(index !== -1) {
+                tasks.splice(index, 1);
+                console.log('Видалено завдання: ', taskText);
+            } else {
+                console.log('Завдання не знайдено: ', taskText);
+            }   
+        }
+    }
+}
+
+// Створення списку завдань
+
+const todoList = createTodoList();
+
+todoList.addTask('task 1');
+todoList.addTask('task 2');
+console.log(todoList.viewTasks());
+todoList.removeTask('task 2');
+console.log(todoList.viewTasks());
+todoList.removeTask('abracadabra');
