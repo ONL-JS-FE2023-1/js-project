5e2372c720bc45ee91ebdc5f3e487f870b560f87
Roman Ratushnyi <freshcode1001@ukr.net>
2023-06-23 18:01:49 +0300

    Home task resolve

--- index.js
+++ index.js
@@ -1,74 +1,30 @@
-// {
-//     {
-//         {
-//             {
-//                 {
-//                     {
+// home task
 
-//                     }
-//                 }
-//                 {
-
-//                 }
-//             }
-//             {
-//                 {
-
-//                 }
-//             }
-//         }
-//         {
-//             {
-
-//             }
-//         }
-//     }
-// }
-
-/* Деструктуризація */
-
-const monitor = {
-    sizes: {
-        height: {
-            value: 30,
-            scale: 'cm'
-        },
-        width: {
-            value: 50,
-            scale: 'cm'
-        }
+const user = {
+    name: 'John',
+    age: 30, 
+    address: {
+        city: 'Kyiv',
+        country: 'Ukraine'
     },
-    brightness: 750,
-    refresh: {
-        value: 144,
-        scale: 'Ggrc'
-    },
-    color: 'white',
-    resolution: '4K'
+    contacts: {
+        email: 'john@example.com',
+        phone: '123456789'
+    }
 }
 
+// name, city, email, phone
 
-// колір
-monitor.color
+const { name: userName,
+        address: {
+            city: userCity
+        },
+        contacts: {
+            email: userEmail, phone: userPhone
+        }
+} = user;
 
-// висота
-monitor.sizes.height
-
-// отримати значення висоти
-monitor.sizes.height.value
-
-// const height = monitor.sizes.height.value;
-
-const {resolution, color: monitorColor} = monitor; // const resolution = monitor.resolution
-
-// Задача: витягнути значення ширина та висоти з об'єкту
-const {sizes: {
-    height: {
-        value: heightValue // const heightValue = monitor.sizes.height.value
-    },
-    width: {
-        value: widthValue // const widthValue = monitor.sizes.width.value
-    }
-}} = monitor;
-
-// ...
+console.log(userName);
+console.log(userCity);
+console.log(userEmail);
+console.log(userPhone);
